7b8e5d82547cbcdf84ab3e380ae5357607291d81
Jared Fuller <jaredshane@gmail.com>
2018-02-07 15:08:04 -0600

    initial commit with a couple exercises I have started

---
+++ intro/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace bangazoncli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime purchaseDate=DateTime.Now;
+
+            string lastName="Smith";
+
+            var firstName="Bill";
+
+            Console.WriteLine($"{firstName} {lastName} purchased on {purchaseDate}");
+
+              string[] products = new string[] {
+                "Motorcycle",
+                "Sofa",
+                "Sandals",
+                "Omega Watch",
+                "iPhone"
+            };
+
+            foreach (string product in products) {
+                if (product.Length > 5){
+                    Console.WriteLine(product);
+                }
+            }
+        }
+    }
+}
